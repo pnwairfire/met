506bdfeb11ac32cf4b1aa5a849722a6597a0cb30
Joel Dubowy <jdubowy@gmail.com>
2018-06-28 22:34:41 -0700

    Fix mispelled index data field name

--- met/arl/arlindexer.py
+++ met/arl/arlindexer.py
@@ -156,7 +156,7 @@ class ArlIndexer(ArlFinder):
         complete_dates = [d for d in dates if len(dates[d]) == 24]
         partial_dates = list(set(dates) - set(complete_dates))
         server_name = self._config.get('server_name') or socket.gethostname()
-        availabilty = self._determine_availability_time_windows(files)
+        availability = self._determine_availability_time_windows(files)
         data = {
             'server': server_name,
             'domain': self._domain,
@@ -167,7 +167,7 @@ class ArlIndexer(ArlFinder):
             'partial_dates': sorted(partial_dates),
             'root_dir': self._met_root_dir,
             'files': files,
-            'availabilty': availabilty
+            'availability': availability
         }
 
         # TODO: slice and dice data in another way?
